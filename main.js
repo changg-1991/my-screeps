5499d46ac374dbb79017f93d8da943490cad1e70
jipi <jipi@lingyongqian.cn>
2017-04-21 20:49:35 +0800

    1.Room大搬家；

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ module.exports.loop = function () {
     runtime.run();
 
     var spawnOrder = new Object();
-    spawnOrder._1Builder = false;
+    spawnOrder._1Builder = true;
 
     structureController.run(config.structure);
     creepController.run(config.creep, spawnOrder);
