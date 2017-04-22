a6c50730ff0f060698455ba466db0e5b88bd164e
jipi <jipi@lingyongqian.cn>
2017-04-22 16:19:17 +0800

    1.bugfix；

--- creep.controller.js
+++ creep.controller.js
@@ -92,13 +92,13 @@ var creepController = {
         if (roleCount._3DiggerRight == null || roleCount._3DiggerRight < 1) {
             var newName = Game.spawns['Spawn3'].createCreep([WORK,WORK,WORK,WORK,WORK,MOVE], undefined, {role: '_3DiggerRight'});
         } else if (roleCount._3CarrierRight == null || roleCount._3CarrierRight < 1) {
-            var newName = Game.spawns['Spawn3'].createCreep([CARRY,CARRY,CARRY,CARRY,MOVE,MOVE], undefined, {role: '_3CarrierRight'});
+            var newName = Game.spawns['Spawn3'].createCreep([CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: '_3CarrierRight'});
         } else if (roleCount._3Upgrader == null || roleCount._3Upgrader < 2) {
             var newName = Game.spawns['Spawn3'].createCreep([WORK,WORK,WORK,WORK,WORK,CARRY,MOVE], undefined, {role: '_3Upgrader'});
         } else if (roleCount._3DiggerLeft == null || roleCount._3DiggerLeft < 1) {
             var newName = Game.spawns['Spawn3'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: '_3DiggerLeft'});
-        } else if (roleCount._3LinkCenter == null || roleCount._3LinkCenter < 1) {
-            var newName = Game.spawns['Spawn3'].createCreep([CARRY,CARRY,MOVE], undefined, {role: '_3LinkCenter'});
+        } else if (roleCount._3CarrierCenter == null || roleCount._3CarrierCenter < 1) {
+            var newName = Game.spawns['Spawn3'].createCreep([CARRY,CARRY,MOVE], undefined, {role: '_3CarrierCenter'});
         } else {
             //
         }
