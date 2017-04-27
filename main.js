655c4482607731f0f60face62a65c69af008ba17
jipi <jipi@lingyongqian.cn>
2017-04-27 13:03:42 +0800

    1.bugfix；

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ module.exports.loop = function () {
     runtime.run();
 
     structureController.run(config.structure);
-    creepController.run(config.creep, spawnOrder);
+    creepController.run(config.creep);
 }
