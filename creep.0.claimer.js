c9b7c47068fcd4150915941f78139abba5715b7a
jipi <jipi@lingyongqian.cn>
2017-05-02 10:07:42 +0800

    1.再来一个屋子；

--- creep.0.claimer.js
+++ creep.0.claimer.js
@@ -1,28 +1,25 @@
 var creepModule = {
+    // Game.spawns['Spawn1'].createCreep([CLAIM,MOVE], undefined, {role: '_0Claimer'});
     body: [CLAIM,MOVE],
     count: 0,
     createType: 'counting',
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.room.name == 'W88S58') {
-            creep.moveTo(25, 49);
-        } else if (creep.room.name == 'W88S59') {
-            creep.moveTo(49, 42);
-        } else if (creep.room.name == 'W87S59') {
-            creep.moveTo(35, 49);
-        } else if (creep.room.name == 'W87S60') {
-            creep.moveTo(49, 8);
-        } else if (creep.room.name == 'W86S60') {
-            creep.moveTo(17, 0);
-        } else if (creep.room.name == 'W86S59') {
+        let targetRoom = 'W99S21';
+
+        if (creep.room.name == targetRoom) {
             if(creep.room.controller) {
                 if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(creep.room.controller);
                 }
             }
         } else {
-            //
+            const route = Game.map.findRoute(creep.room, targetRoom);
+            if(route.length > 0) {
+                const exit = creep.pos.findClosestByRange(route[0].exit);
+                creep.moveTo(exit);
+            }
         }
     }
 };
