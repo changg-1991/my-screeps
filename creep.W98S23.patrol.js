9b69090690029255347cb5aedb8aea6729cc9df5
jipi <jipi@lingyongqian.cn>
2017-05-05 09:36:05 +0800

    1.启用巡逻队；

--- creep.W98S23.patrol.js
+++ creep.W98S23.patrol.js
@@ -1,11 +1,11 @@
 var creepModule = {
     body: [TOUGH,TOUGH,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,HEAL,HEAL],
-    count: 0,
+    count: 2,
     createType: 'counting',
 
     run: function(creep) {
-        let startRoom = 'W98S23';
-        let endRoom = 'W96S23';
+        var startRoom = 'W98S23';
+        var endRoom = 'W98S25';
 
         if (creep.room.name == startRoom) {
             creep.memory.status = 'GOING';
@@ -15,13 +15,23 @@ var creepModule = {
             creep.memory.status = 'COMMING';
         }
 
-        if (creep.memory.status == 'GOING') {
-            var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
-                filter: function(object) {
-                    return Memory.ally.indexOf(object.owner) == -1;
-                }
-            });
+        if (creep.getActiveBodyparts(ATTACK) == 0) {
+            creep.memory.status = 'ESCAPE';
+        }
 
+        if (creep.memory.status = 'ESCAPE' && object.hits == object.hitsMax) {
+            creep.memory.status = 'GOING';
+        }
+
+        creep.moveTo(25,36);
+
+        /*var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
+            filter: function(object) {
+                return Memory.ally.indexOf(object.owner) == -1;
+            }
+        });
+
+        if (creep.memory.status == 'GOING') {
             if (closestHostile) {
                 if(creep.attack(closestHostile) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(closestHostile);
@@ -34,13 +44,8 @@ var creepModule = {
                     creep.moveTo(exit);
                 }
             }
-        } else {
-            var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
-                filter: function(object) {
-                    return Memory.ally.indexOf(object.owner) == -1;
-                }
-            });
-
+            creep.heal(creep);
+        } else if (creep.memory.status == 'COMMING') {
             if (closestHostile) {
                 if(creep.attack(closestHostile) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(closestHostile);
@@ -53,7 +58,26 @@ var creepModule = {
                     creep.moveTo(exit);
                 }
             }
-        }
+            creep.heal(creep);
+        } else if (creep.memory.status == 'ESCAPE') {
+            if (closestHostile) {
+                const range = creep.pos.getRangeTo(closestHostile);
+                if (range > 3) {
+                    creep.heal(creep);
+                } else {
+                    const route = Game.map.findRoute(creep.room, startRoom);
+                    if (route.length > 0) {
+                        const exit = creep.pos.findClosestByRange(route[0].exit);
+                        PathFinder.use(true);
+                        creep.moveTo(exit);
+                    }
+                }
+            } else {
+                creep.heal(creep);
+            }
+        } else {
+
+        }*/
     }
 };
 
