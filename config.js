d3bac9e964db28e486ab33aec5cbcac78352ceb2
jipi <jipi@lingyongqian.cn>
2017-05-19 06:37:25 +0800

    1.xxx;

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ var config = new Object();
 config.creep = new Object();
 // W94S29
 config.creep.W94S29_roleList = [
-    'W94S29_farmerRight', 'W94S29_farmerLeft', 'W94S29_upgraderRight', 'W94S29_upgraderLeft', 'W94S29_builder'
+    'W94S29_farmerRight', 'W94S29_farmerLeft', 'W94S29_diggerRight', 'W94S29_diggerLeft',
+    'W94S29_upgraderRight', 'W94S29_upgraderLeft'
 ];
 
 for (const i in config.creep) {
