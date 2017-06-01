8573f22c85afd713322ddc46f4bba9ec924f0102
jipi <jipi@lingyongqian.cn>
2017-06-01 11:10:15 +0800

    123

--- creep.W92S29.diggerRight.js
+++ creep.W92S29.diggerRight.js
@@ -17,7 +17,7 @@ var creepModule = {
     },
 
     getBody: function(roomName) {
-        return [WORK,WORK,WORK,MOVE,MOVE];
+        return [WORK,WORK,WORK,WORK,WORK,MOVE,MOVE];
     },
 
     getCount: function(roomName) {
--- creep.W94S28.diggerDown.js
+++ creep.W94S28.diggerDown.js
@@ -5,19 +5,19 @@ var creepModule = {
         if (creep.room.name == targetRoom) {
             var source = Game.getObjectById(Memory.objectId.W94S28_sourceDown);
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            }
-        } else {
-            const route = Game.map.findRoute(creep.room, targetRoom);
-            if (route.length > 0) {
-                const exit = creep.pos.findClosestByRange(route[0].exit);
-                creep.moveTo(exit, {
+                creep.moveTo(source, {
                     costCallback: function(roomName, costMatrix) {
                         if (roomName == 'W94S28') {
                             costMatrix.set(14, 45, 0);
                         }
                     }
                 });
+            }
+        } else {
+            const route = Game.map.findRoute(creep.room, targetRoom);
+            if (route.length > 0) {
+                const exit = creep.pos.findClosestByRange(route[0].exit);
+                creep.moveTo(exit);
             }
         }
     },
