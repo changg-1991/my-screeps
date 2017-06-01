98ccf6f192b16098efced9464932e1d627cabec2
jipi <jipi@lingyongqian.cn>
2017-06-01 10:36:30 +0800

    123

--- config.js
+++ config.js
@@ -6,12 +6,11 @@ config.creep = new Object();
 config.creep.W94S29_roleList = [
     'W94S29_farmer', 'W94S29_diggerRight', 'W94S29_diggerLeft', 'W94S29_artillery',
     'W94S29_upgraderRight', 'W94S29_upgraderLeft', 'W95S29_reserver', 'W95S29_digger',
-    'W95S29_carrier', 'W94S29_builder', 'W94S29_destroyer', 'W94S29_fighter', 'W94S29_healler',
-    'W92S29_diggerLeft', 'W92S29_builder', 'W94S29_destroyer2', 'W94S29_healler2'
+    'W95S29_carrier', 'W94S29_builder', 'W94S28_diggerUp', 'W94S28_diggerDown', 'W94S28_builder'
 ];
 // W92S29
 config.creep.W92S29_roleList = [
-    'W92S29_carrier', 'W92S29_diggerRight', 'W92S29_farmer', 'W92S29_builder2'
+    'W92S29_carrier', 'W92S29_diggerRight', 'W92S29_diggerLeft', 'W92S29_farmer', 'W92S29_builder'
 ];
 for (const i in config.creep) {
     let roleList = config.creep[i]
@@ -38,6 +37,12 @@ Memory.objectId.W94S29_storage = '592093fcd1c41e5ee8957af0';
 
 Memory.objectId.W92S29_sourceLeft = '58dbc3188283ff5308a3d07a';
 Memory.objectId.W92S29_sourceRight = '58dbc3188283ff5308a3d079';
+
+Memory.objectId.W94S28_sourceUp = '58dbc2df8283ff5308a3cae4';
+Memory.objectId.W94S28_sourceDown = '58dbc2df8283ff5308a3cae5';
+
+Memory.objectId.W92S28_sourceUp = '58dbc3188283ff5308a3d075';
+Memory.objectId.W92S28_sourceDown = '58dbc3188283ff5308a3d077';
 
 Memory.ally = new Array('kikooo', 'Time_Shift');
 
--- creep.W92S29.builder.js
+++ creep.W92S29.builder.js
@@ -1,6 +1,8 @@
 var creepModule = {
 
     run: function(creep) {
+        var energyFull = creep.room.energyAvailable < creep.room.energyCapacityAvailable ? false : true;
+
         if (creep.memory.status != 'PACKING' && creep.carry.energy == 0) {
             creep.memory.status = 'PACKING';
         }
@@ -9,41 +11,49 @@ var creepModule = {
             delete creep.memory.packingTarget;
         }
 
-        let targetRoom = 'W92S29';
-        if (creep.room.name == targetRoom) {
-            if (creep.memory.status == 'PACKING') {
-                if (!creep.memory.packingTarget || creep.memory.packingTargetTimeOut < Game.time) {
-                    var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                            return structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 1000;
+        if (creep.memory.status == 'PACKING') {
+            if (!creep.memory.packingTarget || creep.memory.packingTargetTimeOut < Game.time) {
+                var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 1000;
+                    }
+                });
+
+                if (!target) {
+                    var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY, {
+                        filter: function(object) {
+                            return object.amount > 300;
                         }
                     });
-
-                    if (!target) {
-                        var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY, {
-                            filter: function(object) {
-                                return object.amount > 300;
-                            }
-                        });
-                    }
-
-                    if (target) {
-                        creep.memory.packingTarget = target.id;
-                        creep.memory.packingTargetTimeOut = Game.time + 8;
-                    }
                 }
 
-                var target = Game.getObjectById(creep.memory.packingTarget);
-                if (target.store) {
-                    if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target);
-                    }
-                } else if (target.resourceType) {
-                    if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target);
-                    }
-                } else {
+                if (target) {
+                    creep.memory.packingTarget = target.id;
+                    creep.memory.packingTargetTimeOut = Game.time + 8;
+                }
+            }
 
+            var target = Game.getObjectById(creep.memory.packingTarget);
+            if (target.store) {
+                if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            } else if (target.resourceType) {
+                if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            } else {
+
+            }
+        } else {
+            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if (target) {
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
                 }
             } else {
                 var constructionSites = Game.rooms['W92S29'].find(FIND_CONSTRUCTION_SITES);
@@ -56,12 +66,6 @@ var creepModule = {
                         creep.moveTo(creep.room.controller);
                     }
                 }
-            }
-        } else {
-            const route = Game.map.findRoute(creep.room, targetRoom);
-            if (route.length > 0) {
-                const exit = creep.pos.findClosestByRange(route[0].exit);
-                creep.moveTo(exit);
             }
         }
     },
