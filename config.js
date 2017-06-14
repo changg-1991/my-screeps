16aa64a995daba099a8db030507bc2b4c3f86f65
jipi <jipi@lingyongqian.cn>
2017-06-14 19:52:19 +0800

    123

--- config.js
+++ config.js
@@ -2,31 +2,10 @@ var config = new Object();
 
 // Creep
 config.creep = new Object();
-// W94S29
-config.creep.W94S29_roleList = [
-    'W94S29_farmer', 'W94S29_diggerRight', 'W94S29_diggerLeft', 'W94S29_artillery',
-    'W94S29_upgraderRight', 'W94S29_upgraderLeft', 'W95S29_reserver', 'W95S29_digger',
-    'W95S29_carrier', 'W94S29_builder', 'W94S28_diggerUp', 'W94S28_diggerDown',
-    'W94S29_miner', 'W94S29_mineralCarrier',
-    'W94S29_destroyer2', 'W94S29_fighter'
-];
-// W92S29
-config.creep.W92S29_roleList = [
-    'W92S29_farmer', 'W92S29_diggerRight', 'W92S29_transfer', 'W92S29_diggerLeft', 'W92S29_carrier', 'W92S29_builder',
-    'W92S29_carrierController', 'W92S29_upgrader'
-];
 
-// W94S28
+// W98S23
 config.creep.W94S28_roleList = [
-    'W94S28_carrier', 'W94S28_farmer', 'W94S28_builder', 'W95S28_reserver', 'W94S27_reserver'
-];
-
-// W92S28
-config.creep.W92S28_roleList = [
-    'W92S28_farmer', 'W92S28_diggerUp', 'W92S28_transfer', 'W92S28_diggerDown', 'W92S28_carrier', 'W92S28_builder',
-    'W91S28_reserver', 'W91S28_digger', 'W91S28_carrier',
-    'W93S28_reserver', 'W93S28_diggerLeft', 'W93S28_carrierLeft'
-
+    'W98S23_builder'
 ];
 
 for (const i in config.creep) {
@@ -46,44 +25,10 @@ config.structure.rampart = require('structure.rampart');
 
 // Object Id
 Memory.objectId = new Object();
-Memory.objectId.W94S29_sourceLeft = '58dbc2e08283ff5308a3cae8';
-Memory.objectId.W94S29_sourceRight = '58dbc2e08283ff5308a3cae9';
-Memory.objectId.W95S29_source = '58dbc2c28283ff5308a3c730';
+Memory.objectId.W98S23_sourceUp = '58dbc2698283ff5308a3bd92';
+Memory.objectId.W98S23_sourceUp = '58dbc2698283ff5308a3bd94';
 
-Memory.objectId.W94S29_storage = '592093fcd1c41e5ee8957af0';
-
-Memory.objectId.W92S29_sourceLeft = '58dbc3188283ff5308a3d07a';
-Memory.objectId.W92S29_sourceRight = '58dbc3188283ff5308a3d079';
-
-Memory.objectId.W94S28_sourceUp = '58dbc2df8283ff5308a3cae4';
-Memory.objectId.W94S28_sourceDown = '58dbc2df8283ff5308a3cae5';
-
-Memory.objectId.W92S28_sourceUp = '58dbc3188283ff5308a3d075';
-Memory.objectId.W92S28_sourceDown = '58dbc3188283ff5308a3d077';
-
-Memory.objectId.W92S29_linkRight = '592f9ea81358b03068899082';
-Memory.objectId.W92S29_linkCenter = '592f8b661c8b90d606019835';
-
-Memory.objectId.W92S29_storage = '592c4dc0195eca196f68cf6d';
-
-Memory.objectId.W91S28_source = '58dbc3328283ff5308a3d2f3';
-
-Memory.objectId.W92S28_storage = '593509f7f6154c0c22a74002';
-
-Memory.objectId.W92S28_linkUp = '593e5258366eee7f7496f88f';
-Memory.objectId.W92S28_linkCenter = '593e3c2e9e987a0215f3991e';
-
-Memory.objectId.W94S28_storage = '5934f94cf6154c0c22a739ec';
-
-Memory.objectId.W94S28_linkUp = '5937990b949bd0931b9a76ed';
-Memory.objectId.W94S28_linkCenter = '5937913944a3c81711ee8210';
-
-Memory.objectId.W93S28_sourceLeft = '58dbc2fa8283ff5308a3cde6';
-Memory.objectId.W93S28_sourceRight = '58dbc2fa8283ff5308a3cde8';
-
-Memory.objectId.W94S29_mineral = '58dbc90334e898064bcc22dc';
-
-Memory.ally = new Array('kikooo', 'Time_Shift');
+Memory.ally = new Array('kikooo', 'Gorgar', 'Time_Shift');
 
 if (!Memory.birthTime) {
     Memory.birthTime = new Object();
---
+++ creep.W98S23.builder.js
@@ -0,0 +1,53 @@
+var creepModule = {
+
+    run: function(creep) {
+        if (creep.memory.status != 'PACKING' && creep.carry.energy == 0) {
+            creep.memory.status = 'PACKING';
+        }
+        if (creep.memory.status != 'BUILDING' && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.status = 'BUILDING';
+            delete creep.memory.packingTarget;
+        }
+
+        if (creep.memory.status == 'PACKING') {
+            const target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+            if(target) {
+                if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        } else {
+            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: function (structure) {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if (target) {
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            } else {
+                var constructionSites = Game.rooms['W92S29'].find(FIND_CONSTRUCTION_SITES);
+                if (constructionSites.length > 0) {
+                    if (creep.build(constructionSites[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(constructionSites[0]);
+                    }
+                }
+            }
+        }
+    },
+
+    getBody: function(roomName) {
+        return [WORK,CARRY,MOVE];
+    },
+
+    getCount: function(roomName) {
+        return 9;
+    },
+
+    getCreateType: function(roomName) {
+        return 'counting';
+    },
+};
+
+module.exports = creepModule;
